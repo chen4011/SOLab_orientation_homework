a4c2f43f5e8781e69ddb3f8d7c0ccf1370f58711
chen4011 <chenpc@solab.me.ntu.edu.tw>
2022-08-24 16:53:00 +0800

    更新目標函數、起始點和上下界和不等式拘束條件係數向量

--- main.m
+++ main.m
@@ -1,11 +1,11 @@
-doc fmincon %利用 doc 查詢 fmincon 的使用方式
-x0=[0.1;0.1]; %起始點
+%doc fmincon %利用 doc 查詢 fmincon 的使用方式
+x0=[0.001;0.001]; %起始點
 A=[-1 -3; 1 3; -1 -3; 1 1]; %線性不等式拘束條件的係數矩陣
-b=[0 18 0 8]; %線性不等式拘束條件的係數向量 AX <= b
+b=[250000000 0.02]; %線性不等式拘束條件的係數向量 AX <= b
 Aeq=[]; %線性不等式拘束條件的係數向量
 beq=[]; %線性等式拘束條件的係數向量 AeqX = beq
-ub=[6;6]; %設計空間的 upper bounds
-lb=[0;0]; %設計空間的 lower bounds
+ub=[0.5;0.5]; %設計空間的 upper bounds
+lb=[0.001;0.001]; %設計空間的 lower bounds
 options = optimset ('display','off','Algorithm','sqp');%演算法的參數設定
 [x,fval,exitflag]=fmincon(@(x)obj(x),x0,A,b,Aeq,beq,lb,ub,...
 @(x)nonlcon(x),options);
